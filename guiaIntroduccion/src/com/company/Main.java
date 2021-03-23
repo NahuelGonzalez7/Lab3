a2a5f86a84cc1b74a55c208ddb4e80573fbf6953
unknown <nahuelgonzalez07@hotmail.es>
2021-03-23 11:50:55 -0300

    Ejercicio 3,4 y 5

--- guiaIntroduccion/src/com/company/Main.java
+++ guiaIntroduccion/src/com/company/Main.java
@@ -2,17 +2,19 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main
-{
+public class Main {
     public static void main(String[] args) {
 // write your code here
         Scanner scan = new Scanner(System.in);
 
-        //int N = 7;
+
+//EJERCICIO 1
+        /*
+        int N = 7;
         double A = 2.10;
         char C = 's';
 
-        /*
+
         System.out.println(N);
         System.out.println(A);
         System.out.println(C);
@@ -20,7 +22,8 @@ public class Main
         System.out.println("Suma de N + A:"+ (A+N));
         System.out.println("A-N: "+(A-N));
         System.out.println("Char --> "+(int)(C));
-        */
+
+//EJERCICIO 2
 
         int X = 4;
         int Y = 5;
@@ -32,12 +35,51 @@ public class Main
         System.out.println(" Suma de N + Y:"+ (N+Y));
         System.out.println(" Multiplicacion de X * N:"+ (X*Y));
 
+        //EJERCICIO 3
+
+        int N = 20;
+        System.out.println("Sumar 77 a N, N ahora es:"+ (N+77));
+        System.out.println("Reducir N a 3, N ahora es:"+ (N-17));
+        System.out.println("Duplicar el valor de N, N ahora es:"+ (N*2));
+
+
+// EJERCICIO 4
+
+        int A = 2;
+        int B = 32;
+        int C = 5;
+        int D = 66;
+        int Aux;
+
+        Aux = B;
+        B = C;
+        C = A;
+        A = D;
+        D = Aux;
+
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+        System.out.println(D);
+    */
+
+        //EJERCICIO 5
+        int A = 5;
+
+        if (A % 2 == 0)
+            {
+                System.out.println("La variable es Par");
+            }
+        else
+            {
+                System.out.println("La variable es Impar");
+            }
+
     }
 
 
 
-    public void Ejercicio1()
-    {
+    public void Ejercicio1() {
         Scanner scan = new Scanner(System.in);
 
         int N;
